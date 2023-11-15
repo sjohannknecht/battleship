2619290ffbe41b5de879720a6646e2cdf702e859
sjohannknecht <sjohannknecht@techfak.uni-bielefeld.de>
2023-11-15 18:27:15 +0100

    Implement messageBroker for separation of concerns between presentation and logic

---
+++ src/messageBroker.js
@@ -0,0 +1,29 @@
+/**
+ * A singleton for aggregating messages sent between different modules. Based on the Publish-Subscribe pattern.
+ * @type {{subscribe(*, *): void, unsubscribe(*, *): void, publish(*, *): void}}
+ */
+
+const messageBroker = (function () {
+    const messages = {};
+
+    return {
+        subscribe(message, fn) {
+            if (!messages[message]) {
+                messages[message] = [];
+            }
+            messages[message].push(fn);
+        },
+        unsubscribe(message, fn) {
+            if (messages[message]) {
+                messages[message] = messages[message].filter(func => func !== fn);
+            }
+        },
+        publish(message, data) {
+            if (messages[message]) {
+                messages[message].forEach(fn => fn(data));
+            }
+        }
+    }
+})();
+
+export default messageBroker;
